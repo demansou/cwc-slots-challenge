9d9fcb1c5310a727d3131aea1a8279d9d2cc06a9
Daniel Mansour <demansou@gmail.com>
2021-12-05 22:52:24 -0600

    Initial Commit

---
+++ slots-challenge/slots_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slots_challengeApp.swift
+//  slots-challenge
+//
+//  Created by Daniel Mansour on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct slots_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
